d097500e76207b66924d74fda77c423acc7640c7
AmadeusGB <1069830494@qq.com>
2024-07-25 13:30:46 -0700

    ```<commit message>Remove timestamp from image saving process```

--- camera_client/image_processing.py
+++ camera_client/image_processing.py
@@ -1,19 +1,6 @@
 import cv2
-from datetime import datetime
 from skimage.metrics import structural_similarity as ssim
 import os
-
-def add_timestamp(frame, timestamp, sequence_number, location):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1.0
-    font_thickness = 2
-    text_color = (0, 255, 0)
-    line_type = cv2.LINE_AA
-
-    cv2.putText(frame, f'Time: {timestamp}', (10, 30), font, font_scale, text_color, font_thickness, line_type)
-    cv2.putText(frame, f'Seq: {sequence_number}', (10, 60), font, font_scale, text_color, font_thickness, line_type)
-    cv2.putText(frame, f'Location: {location}', (10, 90), font, font_scale, text_color, font_thickness, line_type)
-    return frame
 
 def detect_motion(curr_gray, bg_subtractor, threshold=60, min_contour_area=4000):
     fg_mask = bg_subtractor.apply(curr_gray)
@@ -32,9 +19,7 @@ def is_similar(image1, image2, similarity_threshold=0.85):
     return score > similarity_threshold
 
 def save_image(frame, save_dir, location, device_name, image_count, image_format, motion_type):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    frame_with_timestamp = add_timestamp(frame.copy(), timestamp, image_count, location)
     save_path = os.path.join(save_dir, f'{location}_{device_name}_{motion_type}_photo_{image_count}.{image_format}')
-    cv2.imwrite(save_path, frame_with_timestamp)
+    cv2.imwrite(save_path, frame)
     print(f"Photo saved as {save_path}")
-    return frame_with_timestamp
+    return frame
